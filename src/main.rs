15477969c0f4cd8ca8b789e72e246f5955a247e0
Riley Stewart <riley.a.stewart@vanderbilt.edu>
2015-01-18 17:11:03 -0800

    added modularity

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,9 @@ mod bitmap;
 fn main() {
 	let i = precise_time_ns();
 	let width = 800;
-	let height = 800;
+	let height = 600;
 	//let bimp = bitmap::Bmp { bmp: bitmap::new("test.txt", 18, 18) };
 	let bimp = bitmap::Bmp { bmp: bitmap::new_rand(width, height) };
     bimp.write_24("out.bmp", width, height);
-    println!("{}", (precise_time_ns() - i)/1000000);
+    println!("elapsed time: {}", (precise_time_ns() - i)/1000000);
 }
