601b77a903dfc4464f9fdf667220598c35a75600
Riley Stewart <riley.a.stewart@vanderbilt.edu>
2015-01-19 17:04:09 -0600

    added display method

--- src/display.rs
+++ src/display.rs
@@ -1,9 +1,35 @@
-extern crate glutin;
-extern crate gl;
+extern crate sdl2;
 
-fn show (pic: Vec<Vec<u32>>) {
-	let window = glutin::Window::new().unwrap();
-	unsafe { window.make_current(); }
-	gl::load_with(|symbol| window.get_proc_address(symbol));
+use self::sdl2::video::{WindowPos, Window, OPENGL};
+use self::sdl2::event::{Event, poll_event};
+use self::sdl2::surface::{Surface};
+
+pub fn show () {
+	sdl2::init(sdl2::INIT_VIDEO);
+
+	let window = match sdl2::video::Window::new("image", WindowPos::PosCentered, WindowPos::PosCentered, 300, 300, OPENGL) {
+		Ok(window) => window,
+		Err(err) => panic!("failed to create window: {}", err)
+	};
 	
+	let surface = match Surface::from_bmp(&Path::new("out.bmp")) {
+		Ok(surface) => surface,
+		Err(err) => panic!("failed to load surface: {}", err)
+	};
+	
+	let screen = match window.get_surface() {
+		Ok(s) => s,
+		Err(err) => panic!("failed to get window surface: {}", err)
+	};
+	
+	screen.blit(&surface, None, None);
+	
+	window.update_surface();
+	'event : loop {
+		match poll_event() {
+			sdl2::event::Event::Quit(_) => break 'event,
+			_ => {}
+		}
+	}
+	sdl2::quit();	
 }
--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,15 @@ extern crate time;
 use time::precise_time_ns;
 
 mod bitmap;
+mod display;
 
 fn main() {
 	let i = precise_time_ns();
-	let width = 600;
-	let height = 600;
+	let width = 300;
+	let height = 300;
 	//let bimp = bitmap::Bmp { bmp: bitmap::new("test.txt", 18, 18) };
 	let bimp = bitmap::Bmp { bmp: bitmap::new_rand(width, height) };
     bimp.write_24("out.bmp", width, height);
     println!("elapsed time: {}", (precise_time_ns() - i)/1000000);
+    display::show();
 }
